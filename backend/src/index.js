6669fc877f872ed55b244b763bb3defb2cbe33ac
ialexanderbrito <xanderzinho26@gmail.com>
2020-04-05 19:36:02 -0300

    Adicionado socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
+
 const routes = require('./routes');
+const { setupWebSocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect(
   'mongodb+srv://alexander:omnistack@cluster0-kjbki.mongodb.net/week10?retryWrites=true&w=majority',
@@ -17,4 +23,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -15,6 +15,7 @@ import {
 } from 'expo-location';
 
 import api from '../services/api';
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket';
 
 function Main({ navigation }) {
   const [devs, setDevs] = useState([]);
@@ -42,6 +43,18 @@ function Main({ navigation }) {
     loadInitialPosition();
   }, []);
 
+  useEffect(() => {
+    subscribeToNewDevs((dev) => setDevs([...devs, dev]));
+  }, [devs]);
+
+  function setupWebsocket() {
+    disconnect();
+
+    const { latitude, longitude } = currentRegion;
+
+    connect(latitude, longitude, techs);
+  }
+
   async function loadDevs() {
     const { latitude, longitude } = currentRegion;
 
@@ -54,6 +67,7 @@ function Main({ navigation }) {
     });
 
     setDevs(response.data.devs);
+    setupWebsocket();
   }
 
   function handleRegionChanged(region) {
